9e2f19c18e7295e63d0974c00ba5d213d000a0a0
ayu0629 <ayushiagarwal2906@gmail.com>
2025-06-20 02:35:17 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import { PORT } from './config/env.js';
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+import connectToDB from './database/mongodb.js';
+import errorMiddleware from './middleware/error.middleware.js';
+import cookieParser from 'cookie-parser';
+
+const app=express();
+
+app.use(express.json()); //this is built-in middleware which handles json data sent in requests
+app.use(express.urlencoded({extended:false})); //helps to process the form data sent via HTML forms in simple format
+app.use(cookieParser);//reads cookie from incoming requests so your app can store user data
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subcriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to Subscription Tracker API!");
+});
+app.listen(PORT, async()=>{
+    console.log(`Subscription Trcaker API is running on http://localhost:${PORT}`)
+    await connectToDB()
+});
+
+export default app;
---
+++ routes/user.routes.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { getUsers,getUser } from "../controllers/user.controllers.js";
+import { authorize } from "../middleware/auth.middleware.js";
+
+const userRouter=Router();
+
+userRouter.get('/',getUsers);
+
+userRouter.get('/:id',authorize,getUser)
+
+userRouter.post('/:id',(req,res)=>{
+    res.send({title:'Create new user'});
+})
+
+userRouter.put('/:id',(req,res)=>{
+    res.send({title:'Update user'});
+})
+
+userRouter.delete('/:id',(req,res)=>{
+    res.send({title:'Delete user'});
+})
+
+export default userRouter;
